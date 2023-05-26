4fb76df7f5964703bdb1f0d9ed4b841174a11b8e
Christian Iversen <ci@iversenit.dk>
2023-05-27 01:27:53 +0200

    * Move octoprint api endpoints into web.api.octoprint

---
+++ web/api/octoprint.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, request, url_for
+from flask import current_app as app
+
+import cli.util
+import web.config
+import web.util
+
+router = Blueprint("octoprint", __name__)
+
+
+@router.get("/version")
+def app_api_version():
+    """
+    Returns the version details of api and server as dictionary
+
+    Returns:
+        A dictionary containing version details of api and server
+    """
+    return {"api": "0.1", "server": "1.9.0", "text": "OctoPrint 1.9.0"}
+
+
+@router.post("/files/local")
+def app_api_files_local():
+    """
+    Handles the uploading of files to Flask server
+
+    Returns:
+        A dictionary containing file details
+    """
+    user_name = request.headers.get("User-Agent", "ankerctl").split(url_for('app_root'))[0]
+
+    no_act = not cli.util.parse_http_bool(request.form["print"])
+
+    if no_act:
+        cli.util.http_abort(409, "Upload-only not supported by Ankermake M5")
+
+    fd = request.files["file"]
+
+    with app.svc.borrow("filetransfer") as ft:
+        try:
+            ft.send_file(fd, user_name)
+        except ConnectionError as E:
+            log.error(f"Connection error: {E}")
+            # This message will be shown in i.e. PrusaSlicer, so attempt to
+            # provide a readable explanation.
+            cli.util.http_abort(
+                503,
+                "Cannot connect to printer!\n" \
+                "\n" \
+                "Please verify that printer is online, and on the same network as ankerctl.\n" \
+                "\n" \
+                f"Exception information: {E}"
+            )
+
+    return {}
